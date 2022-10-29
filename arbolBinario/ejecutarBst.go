74b4858af48ce6fc7b9bd377b1e46bb9daa48229
DeynerNB <deynernavarrob@gmail.com>
2022-10-29 15:11:25 -0600

    Crear e insertar ABB

--- arbolBinario/ejecutarBst.go
+++ arbolBinario/ejecutarBst.go
@@ -1,9 +1,25 @@
 package main
 
-import "lenguajes/arbolBinario/modulos"
-
-// Prueba de push Github
-
+import (
+	"fmt"
+	"lenguajes/arbolBinario/modulos"
+)
 func main(){
-	bst.FuncionPlaceHolder()
+	// Create instance of BinaryTree
+	BT_Root := &bst.BinaryTree{};
+
+	// Insert elements
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 8, BT_Root.Insert(8));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 3, BT_Root.Insert(3));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 6, BT_Root.Insert(6));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 4, BT_Root.Insert(4));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 1, BT_Root.Insert(1));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 10, BT_Root.Insert(10));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 14, BT_Root.Insert(14));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 13, BT_Root.Insert(13));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 7, BT_Root.Insert(7));
+
+	// Print inorder (Solo pa probar)
+	fmt.Println("\n\nKey\tValue\tL_Node\tR_Node");
+	BT_Root.Root.Print_Inorder();
 }
--- arbolBinario/modulos/arbolBst.go
+++ arbolBinario/modulos/arbolBst.go
@@ -2,6 +2,65 @@ package bst
 
 import "fmt"
 
-func FuncionPlaceHolder() {
-	fmt.Println("Funciones del BST")
+// Struct: Node of a Binary Tree
+type BT_Node struct {
+	Key int;
+	Counter int;
+	Left_node  *BT_Node;
+	Right_node *BT_Node;
 }
+
+// Struct: Root of Binary Tree
+type BinaryTree struct {
+	Root *BT_Node;
+}
+
+// Accesible function to insert element in the tree -> Return the number of comparison made
+func (this *BinaryTree) Insert(key int) (num_Comparison int){
+	num_Comparison = 1;
+	if (this.Root == nil) {
+		this.Root = &BT_Node{ key, 1, nil, nil };
+	} else {
+		insertNode(&this.Root, key, &num_Comparison);
+	}
+	return;
+}
+
+// Inside function to perform the recursive process
+func insertNode(this **BT_Node, new_key int, num_Comparison *int) {
+	if ((*this) == nil) {
+		(*num_Comparison) += 1;
+		*this = &BT_Node{ new_key, 1, nil, nil };
+
+	} else if(new_key == (*this).Key) {
+		(*num_Comparison) += 2;
+		(*this).Counter++;
+
+	} else if(new_key < (*this).Key) {
+		(*num_Comparison) += 3;
+		insertNode( &((*this).Left_node), new_key, num_Comparison );
+
+	} else {
+		(*num_Comparison) += 3;
+		insertNode( &((*this).Right_node), new_key, num_Comparison );
+	}
+}
+
+// Test function: Show every value of every node in the tree
+func (this *BT_Node) Print_Inorder() {
+	if (this != nil) {
+		var L_Value int = -1;
+		var R_Value int = -1;
+
+		if (this.Left_node != nil) {
+			L_Value = this.Left_node.Key;
+		}
+		if (this.Right_node != nil) {
+			R_Value = this.Right_node.Key;
+		}
+
+		this.Left_node.Print_Inorder();
+		fmt.Printf("%v\t%v\t%v\t%v\n", this.Key, this.Counter, L_Value, R_Value);
+		this.Right_node.Print_Inorder();
+	}
+} 
