c11a8526bb426d7983df6df94b938a7cd404f0a5
iHuskyx <andrealh17@estudiantec.cr>
2022-11-04 16:45:16 -0600

    correccion docu interna

--- GeneradorPseudo-Aleatorios/ejecutarGenerador.go
+++ GeneradorPseudo-Aleatorios/ejecutarGenerador.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	gen := gen.GeneradorParametros{Semilla: 11, N: 500, X: 2040, Limit: 500}
 
-	// Cada vez que se hace Next se consigue el siguiente numero generado
+	// Cada vez que se llama al método se consigue el siguiente numero generado
 	for {
 		result := gen.Generador()
 		if result == 0 {
