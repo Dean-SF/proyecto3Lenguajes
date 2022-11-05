ca693fef7d365920b1f115f61a11fb5a3f8f7fcb
DeynerNB <deynernavarrob@gmail.com>
2022-11-04 20:48:16 -0600

    Pruebas individuales

--- arbolBinario/ejecutarBst.go
+++ arbolBinario/ejecutarBst.go
@@ -10,55 +10,43 @@ func main() {
 	BT_Root := &bst.BinaryTree{}
 
 	// Insert elements
-	// fmt.Printf("\nComparisons made until %d was inserted: %d", 8, BT_Root.Insert(8));
-	// fmt.Printf("\nComparisons made until %d was inserted: %d", 3, BT_Root.Insert(3));
-	// fmt.Printf("\nComparisons made until %d was inserted: %d", 6, BT_Root.Insert(6));
-	// fmt.Printf("\nComparisons made until %d was inserted: %d", 4, BT_Root.Insert(4));
-	// fmt.Printf("\nComparisons made until %d was inserted: %d", 1, BT_Root.Insert(1));
-	// fmt.Printf("\nComparisons made until %d was inserted: %d", 10, BT_Root.Insert(10));
-	// fmt.Printf("\nComparisons made until %d was inserted: %d", 14, BT_Root.Insert(14));
-	// fmt.Printf("\nComparisons made until %d was inserted: %d", 13, BT_Root.Insert(13));
-	// fmt.Printf("\nComparisons made until %d was inserted: %d", 7, BT_Root.Insert(7));
-
-	BT_Root.Insert(24)
-	BT_Root.Insert(8)
-	BT_Root.Insert(29)
-	BT_Root.Insert(27)
-	BT_Root.Insert(33)
-	BT_Root.Insert(6)
-	BT_Root.Insert(20)
-	BT_Root.Insert(5)
-	BT_Root.Insert(7)
-	BT_Root.Insert(9)
-	BT_Root.Insert(3)
-	BT_Root.Insert(2)
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 8, BT_Root.Insert(8));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 3, BT_Root.Insert(3));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 6, BT_Root.Insert(6));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 4, BT_Root.Insert(4));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 1, BT_Root.Insert(1));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 10, BT_Root.Insert(10));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 14, BT_Root.Insert(14));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 13, BT_Root.Insert(13));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 7, BT_Root.Insert(7));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 6, BT_Root.Insert(6));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 6, BT_Root.Insert(6));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 10, BT_Root.Insert(10));
+	fmt.Printf("\nComparisons made until %d was inserted: %d", 14, BT_Root.Insert(14));
 
 	// Print inorder (Solo pa probar)
-	fmt.Println("\n\nKey\tValue\tL_Node\tR_Node")
+	fmt.Println("\n\nInorder traversal\nKey\tCount\tL_Node\tR_Node")
 	BT_Root.Root.Print_Inorder()
 
-	fmt.Printf("\nAltura: %v\n", BT_Root.GetHeight());
-	fmt.Printf("\nNodos: %v\n", BT_Root.GetNumberNodes());
+	// Test Find function with a key that exists
+	tupla := BT_Root.Find(13)
+	fmt.Print("\nResultado de Find para Key = 13 : (")
+	fmt.Print(tupla.Found)
+	fmt.Print(",")
+	fmt.Print(tupla.Num_Comparison)
+	fmt.Print(")")
+	fmt.Println()
 
-	// Execute TreeToVine
+	// Test Find function with a key that doesn't exist
+	tupla = BT_Root.Find(50)
+	fmt.Print("Resultado de Find para Key = 50 : (")
+	fmt.Print(tupla.Found)
+	fmt.Print(",")
+	fmt.Print(tupla.Num_Comparison)
+	fmt.Print(")\n")
+
+	// Execute DSW algorithm
 	BT_Root.DSW_Algorithm()
-	fmt.Println("\n\nKey\tValue\tL_Node\tR_Node")
+	fmt.Println("\n\nInorder traversal after DSW\nKey\tValue\tL_Node\tR_Node")
 	BT_Root.Root.Print_Inorder()
-
-	// // Test Find function with a key that exists
-	// tupla := BT_Root.Find(27)
-	// fmt.Print("Resultado de Find para Key = 27 : (")
-	// fmt.Print(tupla.Found)
-	// fmt.Print(",")
-	// fmt.Print(tupla.Num_Comparison)
-	// fmt.Print(")")
-	// fmt.Println()
-
-	// // Test Find function with a key that doesn't exist
-	// tupla = BT_Root.Find(50)
-	// fmt.Print("Resultado de Find para Key = 50 : (")
-	// fmt.Print(tupla.Found)
-	// fmt.Print(",")
-	// fmt.Print(tupla.Num_Comparison)
-	// fmt.Print(")")
 }
